4137086133e47375a8fd74c73b33793a801c1183
Ваня <undeadkivva@mail.ru>
2023-05-17 13:42:57 +0300

    stat_svc

--- stats/stats-server/src/main/java/ru/practicum/stats/stats/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/stats/StatsRepository.java
@@ -13,19 +13,20 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
 
-    @Query(value = "select new  ru.practicum.stats.dto.ViewedEndpointHitDto(e.app,e.uri ,count(e.ip)) from EndpointHit  as e " +
-            "where e.instant between ?1 and ?2 group by e.app, e.uri order by count(e.ip) desc")
+    @Query(value = "select new  ru.practicum.stats.dto.ViewedEndpointHitDto(e.app,e.uri ,count(e.ip))" +
+            " from EndpointHit  as e where e.instant between ?1 and ?2 group by e.app, e.uri order by count(e.ip) desc")
      List<ViewedEndpointHitDto> findViewedEndpointHitDto(LocalDateTime start, LocalDateTime end);
-    @Query(value = "select  new  ru.practicum.stats.dto.ViewedEndpointHitDto(e.app, e.uri , count(e.ip)) from EndpointHit  as e " +
-            "where e.instant between ?1 and ?2 and e.uri in ?3 group by e.app, e.uri order by count(e.ip) desc")
+    @Query(value = "select  new  ru.practicum.stats.dto.ViewedEndpointHitDto(e.app, e.uri , count(e.ip))" +
+            " from EndpointHit  as e where e.instant between ?1 and ?2 and e.uri in ?3" +
+            " group by e.app, e.uri order by count(e.ip) desc")
     List<ViewedEndpointHitDto> findViewedEndpointHitDto(LocalDateTime start, LocalDateTime end, String[] uris);
     @Query(value = "select new  ru.practicum.stats.dto.ViewedEndpointHitDto(e.app, e.uri , count(distinct(e.ip))) " +
-            " from EndpointHit  as e " +
-            "where e.instant between ?1 and ?2 group by e.app, e.uri order by count(distinct(e.ip)) desc")
+            " from EndpointHit  as e where e.instant between ?1 and ?2 " +
+            "group by e.app, e.uri order by count(distinct(e.ip)) desc")
     List<ViewedEndpointHitDto> findViewedEndpointHitDtoWithUniqueIps(LocalDateTime start, LocalDateTime end);
     @Query(value = "select   new  ru.practicum.stats.dto.ViewedEndpointHitDto(e.app, e.uri , count(distinct(e.ip)))" +
-            " from EndpointHit  as e " +
-            "where e.instant between ?1 and ?2 and e.uri in ?3 group by e.app, e.uri order by count(distinct(e.ip)) desc")
+            " from EndpointHit  as e where e.instant between ?1 and ?2 and e.uri in ?3 " +
+            "group by e.app, e.uri order by count(distinct(e.ip)) desc")
     List<ViewedEndpointHitDto> findViewedEndpointHitDtoWithUniqueIps(LocalDateTime start, LocalDateTime end,
                                                                      String[] uris);
 }
